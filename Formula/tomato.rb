3c1c9df82324adba4c407bf6fe5e57ebf7764d89
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-02-14 21:43:55 +0100

    fix sha in version 0.2.0

--- Formula/tomato.rb
+++ Formula/tomato.rb
@@ -2,7 +2,7 @@ class Tomato < Formula
   desc "Set Slack status and availability from the command line"
   homepage "https://github.com/rustamyusupov/tomato"
   url "https://github.com/rustamyusupov/tomato/archive/v0.2.0.tar.gz"
-  sha256 "abbab6d34c6fb2abf5d54787fd6b6ecb63c27e75"
+  sha256 "1a376198881892b9e644b7d01439415cf8ecabc36511934bed617751b10edab5"
 
   depends_on "erlang"
   depends_on "rebar" => :build
